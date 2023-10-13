6cd86bd907d39057a2a376f45f502de4a810efbd
mohamedmehdigara <garamehdi57@yahoo.com>
2023-10-13 11:08:41 +0100

    edited files

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ const AppContainer = styled.div`
 `;
 
 const Pond = styled.div`
-  width: 300px;
-  height: 200px;
+  width: 100%;
+  max-width: 600px;
+  height: 50vh;
   background: #00a8ff;
   border: 5px solid #0097e6;
   position: relative;
@@ -20,6 +21,23 @@ const Pond = styled.div`
 
 const Score = styled.p`
   font-size: 24px;
+  margin-top: 20px;
+`;
+
+const GameOverScreen = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const PlayAgainButton = styled.button`
+  font-size: 18px;
+  background: #f39c12;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
   margin-top: 20px;
 `;
 
@@ -31,6 +49,7 @@ const App = () => {
   ]);
   const [score, setScore] = useState(0);
   const [timeLeft, setTimeLeft] = useState(30);
+  const [gameOver, setGameOver] = useState(false);
 
   const moveFish = () => {
     const newFish = fish.map((f) => ({
@@ -49,28 +68,49 @@ const App = () => {
     setScore(score + 1);
   };
 
+  const handlePlayAgain = () => {
+    setFish([
+      { id: 1, position: 0 },
+      { id: 2, position: 0 },
+      { id: 3, position: 0 },
+    ]);
+    setScore(0);
+    setTimeLeft(30);
+    setGameOver(false);
+  };
+
   useEffect(() => {
-    moveFish();
-    const timer = setInterval(() => {
-      if (timeLeft > 0) {
+    if (timeLeft > 0 && !gameOver) {
+      moveFish();
+      const timer = setTimeout(() => {
         setTimeLeft(timeLeft - 1);
-      } else {
-        clearInterval(timer);
-      }
-    }, 1000);
-  }, [timeLeft]);
+      }, 1000);
+      return () => clearTimeout(timer);
+    } else if (!gameOver) {
+      setGameOver(true);
+    }
+  }, [timeLeft, gameOver]);
 
   return (
     <AppContainer>
-      <Pond>
-        {fish.map((f) => (
-          <Fish key={f.id} id={f.id} position={f.position} onClick={catchFish} />
-        ))}
-      </Pond>
-      <Score>Score: {score}</Score>
-      <p>Time Left: {timeLeft} seconds</p>
+      {gameOver ? (
+        <GameOverScreen>
+          <h1>Game Over!</h1>
+          <Score>Final Score: {score}</Score>
+          <PlayAgainButton onClick={handlePlayAgain}>Play Again</PlayAgainButton>
+        </GameOverScreen>
+      ) : (
+        <>
+          <Pond>
+            {fish.map((f) => (
+              <Fish key={f.id} id={f.id} position={f.position} onClick={catchFish} />
+            ))}
+          </Pond>
+          <Score>Score: {score}</Score>
+          <p>Time Left: {timeLeft} seconds</p>
+        </>
+      )}
     </AppContainer>
   );
-};
-
+            }
 export default App;
